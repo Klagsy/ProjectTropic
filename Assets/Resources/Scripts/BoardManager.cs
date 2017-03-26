28df2287ba2a229a99b83856658e55253bf4dbdb
Eamon Leonard <theklagskast@gmail.com>
2017-03-25 20:24:07 -0700

    Board Scripting

---
+++ Assets/Resources/Scripts/BoardManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour {
+
+    public int columns = 8;
+    public int rows = 8;
+    public GameObject FloorTile;
+    public GameObject WallTile;
+    public GameObject OuterWallTile;
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+    // Use this for initialization
+    void InitialiseList()
+    {
+        //Clear our list gridPositions.
+        gridPositions.Clear();
+
+        //Loop through x axis (columns).
+        for (int x = 1; x < columns - 1; x++)
+        {
+            //Within each column, loop through y axis (rows).
+            for (int y = 1; y < rows - 1; y++)
+            {
+                //At each index add a new Vector3 to our list with the x and y coordinates of that position.
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+    Vector3 RandomPosition()
+    {
+        //Declare an integer randomIndex, set it's value to a random number between 0 and the count of items in our List gridPositions.
+        int randomIndex = Random.Range(0, gridPositions.Count);
+
+        //Declare a variable of type Vector3 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
+        Vector3 randomPosition = gridPositions[randomIndex];
+
+        //Remove the entry at randomIndex from the list so that it can't be re-used.
+        gridPositions.RemoveAt(randomIndex);
+
+        //Return the randomly selected Vector3 position.
+        return randomPosition;
+    }
+    void LayoutObjectAtRandom(GameObject x, int y)
+    {
+
+        //Instantiate objects until the randomly chosen limit objectCount is reached
+        for (int i = 0; i < y; i++)
+        {
+            //Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
+            Vector3 randomPosition = RandomPosition();
+
+            //Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
+            Instantiate(x, randomPosition, Quaternion.identity);
+        }
+    }
+    void BoardSetup () {
+        boardHolder = new GameObject("Board").transform;
+        for (int x = -1; x < columns + 1; x++)
+        {
+            //Loop along y axis, starting from -1 to place floor or outerwall tiles.
+            for (int y = -1; y < rows + 1; y++)
+            {
+                //Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
+                GameObject toInstantiate = FloorTile;
+
+                //Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                    toInstantiate = OuterWallTile;
+
+                if ( y == 3 && x == 3)
+                    toInstantiate = WallTile;
+                GameObject instance =
+                    Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	public void SetupScene() {
+        BoardSetup();
+        InitialiseList();
+	}
+}
---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    private BoardManager boardScript;
+
+	// Use this for initialization
+	void Awake () {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+	
+	// Update is called once per frame
+	void InitGame () {
+        boardScript.SetupScene();
+	
+	}
+    void Update ()
+    {
+
+    }
+}
